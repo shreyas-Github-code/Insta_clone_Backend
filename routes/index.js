1506fa4a69600564e22cac1084cf858e12f1733b
shreyas-Github-code <mishrikotishreyas@gmail.com>
2024-01-21 12:22:59 +0530

    2nd commit

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const UserModel = require('./users');
+const passport = require('passport');
+const localStrategy = require('passport-local');
+
+passport.use(new localStrategy(UserModel.authenticate()));
 
 router.get('/', function(req, res) {
   res.render('index', {footer: false});
@@ -28,5 +33,39 @@ router.get('/edit', function(req, res) {
 router.get('/upload', function(req, res) {
   res.render('upload', {footer: true});
 });
+
+router.post('/register', function(req, res) {
+  const userData=new UserModel({
+    username:req.body.username,
+  name:req.body.name,
+  email:req.body.email,
+  });
+
+  UserModel.register(userData,req.body.password)
+  .then(function(){
+    passport.authenticate('local')(req,res,function(){
+      res.redirect('/profile')
+    })
+  })
+});
+
+router.post('/login', passport.authenticate('local',{
+  successRedirect:'/profile',
+  failureRedirect:'login'
+}),function(req, res) {
+  res.render('upload', {footer: true});
+});
+
+router.get('/logout',(req,res)=>{
+  req.logOut((err)=>{
+    if(err){return next(err);}
+    res.redirect('/login')
+  })
+})
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()) return next();
+  res.redirect('/login');
+}
 
 module.exports = router;
